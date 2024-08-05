cc7d1f64210d1aeac1a0c6d7895b1bc597c00f7c
MELindley <dev.lesbatons@gmail.com>
2024-08-05 17:27:44 +0200

    Replaced state handlers with Redux logic

--- src/components/AthleteCard.tsx
+++ src/components/AthleteCard.tsx
@@ -1,83 +1,91 @@
-import type { KeyboardEvent, ReactElement } from 'react'
+import { type KeyboardEvent, type ReactElement, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import type { IAthlete } from 'types'
-import { useMediaQuery } from 'utils'
-import ImageAttribution from './ImageAttribution'
-import { Button } from '@mui/material'
+import {
+	Button,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Typography
+} from '@mui/material'
+import { useAppDispatch } from 'app/hooks'
+import { addAthlete, removeAthlete } from '../features/battle/battleSlice'
 
 const PREFERRED_IMAGE_WIDTH = 384
 const MOBILE_PADDING = 16
 const ASPECT_RATIO_WIDTH = 16
 const ASPECT_RATIO_HEIGHT = 9
-const IMAGE_INDEX_BELOW_THE_FOLD = 3
 
 interface Properties {
 	athlete: IAthlete
-	index: number
 }
 
-export default function AthleteCard({
-	athlete,
-	index
-}: Properties): ReactElement {
-	const isTabletAndUp = useMediaQuery('(min-width: 600px)')
-
+export default function AthleteCard({ athlete }: Properties): ReactElement {
 	const navigate = useNavigate()
-	function onDetailClick(): void {
+	const dispatch = useAppDispatch()
+	const [isSelected, setIsSelected] = useState<boolean>(false)
+
+	function onDetailButtonClick(): void {
 		window.scrollTo(0, 0)
 		navigate(athlete.name.toLowerCase())
 	}
 
-	function onKeyDown(event: KeyboardEvent): void {
-		if (event.key === 'Enter') {
-			onDetailClick()
+	function onCardClick(): void {
+		if (isSelected) {
+			dispatch(removeAthlete(athlete))
+			setIsSelected(false)
+		} else {
+			dispatch(addAthlete(athlete))
+			setIsSelected(true)
 		}
 	}
 
+	function onKeyDown(event: KeyboardEvent): void {
+		if (event.key === 'Enter') {
+			onCardClick()
+		}
+	}
 	const imageWidth = Math.min(
 		PREFERRED_IMAGE_WIDTH,
 		window.innerWidth - MOBILE_PADDING
 	)
 	const imageHeight = imageWidth / (ASPECT_RATIO_WIDTH / ASPECT_RATIO_HEIGHT)
-
 	return (
-		<div
-			data-testid='AthleteCard'
-			className='cursor-pointer select-none overflow-hidden rounded-lg shadow-lg focus:border-gray-300 focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-50 dark:shadow-2xl'
-			role='button'
-			tabIndex={0}
-			onClick={onDetailClick}
-			onKeyDown={onKeyDown}
+		<Card
+			sx={{
+				maxWidth: imageWidth,
+				border: isSelected ? 'solid blue' : 'solid transparent'
+			}}
 		>
-			<div className='relative'>
-				<img
-					data-testid='AthleteCardImage'
-					loading={
-						!isTabletAndUp && index >= IMAGE_INDEX_BELOW_THE_FOLD
-							? 'lazy'
-							: 'eager'
-					}
-					decoding={
-						!isTabletAndUp && index >= IMAGE_INDEX_BELOW_THE_FOLD
-							? 'async'
-							: 'sync'
-					}
-					width={imageWidth}
-					height={imageHeight}
-					style={{
+			<CardActionArea onClick={onCardClick} onKeyDown={onKeyDown}>
+				<CardMedia
+					component='img'
+					sx={{
+						width: imageWidth,
+						height: imageHeight,
 						backgroundColor: athlete.image.color
 					}}
-					src={athlete.image.url}
+					image={athlete.image.url}
+					title='green iguana'
 					alt={athlete.name}
 				/>
-				<ImageAttribution author={athlete.image.author} />
-			</div>
-			<h3 data-testid='AthleteCardName' className='p-6 text-xl font-bold'>
-				{athlete.name} {athlete.surname}
-			</h3>
-			<Button onClick={onDetailClick} variant='outlined'>
-				Details
-			</Button>
-		</div>
+				<CardContent>
+					<Typography gutterBottom variant='h3' component='div'>
+						{athlete.name} {athlete.surname}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button
+						onClick={onDetailButtonClick}
+						variant='outlined'
+						className='m-auto'
+					>
+						Details
+					</Button>
+				</CardActions>
+			</CardActionArea>
+		</Card>
 	)
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,9 @@ import Head from 'components/Head'
 import LoadingOrError from 'components/LoadingOrError'
 import type { ReactElement } from 'react'
 import NavBar, { type NavigationItem } from '../components/Navbar'
+import { useAppSelector } from 'app/hooks'
+import { selectBattleAthletes } from '../features/battle/battleSlice'
+import type { RootState } from '../app/store'
 
 export const mainNavigation: NavigationItem[] = [{ name: 'Home', href: '/' }]
 
@@ -13,6 +16,11 @@ export default function HomePage(): ReactElement {
 		queryKey: ['athletes'],
 		queryFn: getAthletes
 	})
+
+	const athletesInBattle = useAppSelector((state: RootState) =>
+		selectBattleAthletes(state)
+	)
+
 	if (isPending || isError) {
 		return <LoadingOrError error={error as Error} />
 	}
@@ -21,12 +29,14 @@ export default function HomePage(): ReactElement {
 			<Head title='Tricking Battle Visualizer' />
 			<NavBar navigation={mainNavigation} />
 			<div className='m-2 grid min-h-screen grid-cols-[minmax(0,384px)] place-content-center gap-2 md:m-0 md:grid-cols-[repeat(2,minmax(0,384px))] xl:grid-cols-[repeat(3,384px)]'>
-				{data.map((athlete, index) => (
-					<AthleteCard
-						key={`AthleteCard-${athlete.name}`}
-						athlete={athlete}
-						index={index}
-					/>
+				{data.map(athlete => (
+					<AthleteCard key={`AthleteCard-${athlete.name}`} athlete={athlete} />
+				))}
+			</div>
+			<div>
+				Athletes in battle:
+				{athletesInBattle.map(athlete => (
+					<div key={athlete.name}>{athlete.name}</div>
 				))}
 			</div>
 		</>
