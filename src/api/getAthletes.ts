cd3a04e1cf96fd9c9dd35512466c2435013bbb89
MELindley <dev.lesbatons@gmail.com>
2024-12-24 00:24:28 +0100

    Remove mock data and related API handlers
    This commit deletes unused mock data files (athletes.json, fruits.json) and their associated handlers, server setup, and API file (getAthletes.ts). Additionally, it updates the pnpm-lock.yaml to reflect the removal of related dependencies. These changes aim to clean up the codebase by eliminating unused assets.

--- src/api/getAthletes.ts
+++
@@ -1,8 +0,0 @@
-import type { IAthlete } from 'app/types'
-
-export default async function getAthletes(): Promise<IAthlete[]> {
-	const response = await fetch(
-		'https://66afc46fb05db47acc5ab941.mockapi.io/athletes'
-	)
-	return response.json() as Promise<IAthlete[]>
-}
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,26 +1,16 @@
-import { useQuery } from '@tanstack/react-query'
 import Head from 'components/common/Head'
-import LoadingOrError from 'components/common/LoadingOrError'
 import type { ReactElement } from 'react'
 import { Link, Navigate, useParams } from 'react-router-dom'
-import getAthletes from 'api/getAthletes'
 import NavBar from 'components/common/Navbar'
 import { mainNavigation } from './Home'
 import Grid from '@mui/material/Grid2'
 import { Stack, Typography } from '@mui/material'
+import { useAppSelector } from '../app/hooks'
+import { selectTournamentAthletes } from '../features/tournament/tournamentSlice'
 
 export default function DetailsPage(): ReactElement {
 	const { athleteName } = useParams()
-
-	const { isPending, isError, error, data } = useQuery({
-		queryKey: ['athlete'],
-		queryFn: getAthletes
-	})
-	if (isPending || isError) {
-		return <LoadingOrError error={error as Error} />
-	}
-
-	const athlete = data.find(
+	const athlete = useAppSelector(state => selectTournamentAthletes(state)).find(
 		f => f.name.toLowerCase() === athleteName?.toLowerCase()
 	)
 	if (!athlete) {
